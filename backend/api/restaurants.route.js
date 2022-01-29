f797cb587fb2dfffb1ea45d254f9341c25b10009
Toto <19580671+tomso11@users.noreply.github.com>
2022-01-29 16:15:49 -0300

    Add reviews CRUD

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import RestaurantsCtrl from "./restaurants.controller.js"
-//import ReviewsCtrl from "./reviews.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
@@ -9,11 +9,11 @@ router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
 router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
 router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
 */
-/*
+
 router
   .route("/review")
   .post(ReviewsCtrl.apiPostReview)
   .put(ReviewsCtrl.apiUpdateReview)
   .delete(ReviewsCtrl.apiDeleteReview)
-*/
+
 export default router
