f46911a028d4e168818ccd11cde80435ad1e05d0
Toto <19580671+tomso11@users.noreply.github.com>
2022-01-29 15:26:36 -0300

    Add basic webserver

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+
+dotenv.config()
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.port || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+      wtimeoutMS: 2500,
+    }
+    )
+    .catch(err => {
+      console.error(err.stack)
+      process.exit(1)
+    })
+    .then(async client => {
+      //await RestaurantsDAO.injectDB(client)
+      //await ReviewsDAO.injectDB(client)
+      app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+      })
+    })
